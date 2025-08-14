20035830824ec95318d8f2e08a5b1f0102c57f9b
Sbrawat <xbrawat@gmail.com>
2025-08-14 11:32:08 +0530

    Basic Hashing: started

---
+++ Basics/L6-basicHashing.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+//Counting Frequencies of Array Elements
+vector<vector<int>> countFrequencies(vector<int>& nums) {
+    // Your code goes here
+    map<int, int>;
+    
+}
+
+
+int main(){
+
+    return 0;
+}
